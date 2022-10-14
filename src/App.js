57ffbcb9c30222555582d6c8b78c6279dfbf7211
longy <longhanzhang999@gmail.com>
2022-10-15 00:01:08 +1030

    github learning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  getData = () => {
+    axios.get('??').then(
+      response => { console.log('success', response.data); },
+      error => { console.log('failed', error); }
+    )
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.getData}>Click me to get Data</button>
+
+      </div>
+    )
+  }
 }
-
-export default App;
