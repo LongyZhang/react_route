67005a20f6ddeb348fd6933a061aa556a2fff5a8
longy <longhanzhang999@gmail.com>
2022-10-26 23:16:39 +1030

    Ant Design learning

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
-import { Button } from 'antd';
+import { Button, DatePicker } from 'antd';
+import { UserSwitchOutlined, SearchOutlined } from '@ant-design/icons';
 import "antd/dist/antd.min.css";
-
+import './App.less';
 
 export default class App extends Component {
+
+  f1 = (date, dateString) => {
+    // console.log(date, dateString);
+    console.log(dateString);
+  }
+
+
   render() {
     return (
       <div>
-        <Button type='primary'>Click me first ant Design</Button>
+        <Button type='primary' >Click me </Button>
+        <Button>default</Button>
+        <Button danger>danger</Button>
+        <Button shape="round" ghost>caonima</Button>
+        <Button type="dashed" icon={<SearchOutlined />}>
+          Search
+        </Button>
+        <DatePicker onChange={(date, dateString) => this.f1(date, dateString)} />
+        {/* <CaretRightOutlined /> */}
+        <UserSwitchOutlined onMouseOver={this.f1} />
       </div>
     )
   }
